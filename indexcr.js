aba89e85781d6e7831fe2089d6bbefffa1ef8675
pkthakur <pradeepthakur66927@gmail.com>
2021-12-10 11:28:26 +0530

    e changes

--- indexcr.js
+++ indexcr.js
@@ -6,10 +6,13 @@ const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 const availableNotes = [2000,500,100,20,10,5,1];
 
+var cash = cashGiven.value;
+var bill = billAmount.value
+
 checkButton.addEventListener("click" ,function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
+        if (cash >= bill) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
@@ -27,11 +30,16 @@ function calculateChange(amountToBeReturned) { //2010
         const numberOfNotes = Math.trunc(
             amountToBeReturned / availableNotes[i]
         );
+        if (numberOfNotes<0){
+            amountToBeReturned = amountToBeReturned % availableNotes[i];
+            noOfNotes[i].innerText = numberOfNotes;
+
+        }
         // 2010/2000 = 1 || 10/500=0
 
         //amount left after the number of notes needed
         amountToBeReturned = amountToBeReturned % availableNotes[i];
-        //2010 % 2000 = 10 || 10 % 500= 10
+        // 2010 % 2000 = 10 || 10 % 500= 10
 
         //updating the no of notes in the table for the current amount
         noOfNotes[i].innerText = numberOfNotes;
